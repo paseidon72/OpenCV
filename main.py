6d770a576427aa2e7e2e125864f9ba98a7497254
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-12-30 23:45:40 +0200

    opencv

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+img = cv2.imread('images/1664717838371.jpg')
+img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+img = cv2.GaussianBlur(img, (9, 9), 0)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.Canny(img, 90, 90)
+kernel = np.ones((5, 5), np.uint8)
+img = cv2.dilate(img, kernel, iterations=1)
+img = cv2.erode(img, kernel, iterations=1)
+#print(img.shape)
+cv2.imshow('Result', img)
+cv2.waitKey(0)
+
+# cap = cv2.VideoCapture('videos/vide20.mp4')
+# cap = cv2.VideoCapture(0)
+# cap.set(3, 500)
+# cap.set(4, 300)
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow('Result', img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
